6fa09d62967ad2c90045ec69df6f5f659487adbe
01Nikhat <160391952+01Nikhat@users.noreply.github.com>
2025-06-19 11:59:34 +0530

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -16,15 +16,18 @@ const Experience = () => {
         {/* Experience 1 */}
         <div className="experienceItem">
           <div className="experienceYear">
-            <p className="year">2023-present</p>
+            <p className="year">Mar 2024- Apr 2025</p>
           </div>
           <div className="experienceContent">
             <h6 className="experienceRole">
               Web Developer - <span className="experienceCompany">INA India Pvt. Ltd</span>
             </h6>
             <p className="experienceDescription">
-              Designed and developed user interfaces for web applications using React.js. Worked closely with backend developers to integrate frontend components with Node.js APIs, implemented responsive design, and optimized frontend performance.
-            </p>
+              • Designed a full-featured restaurant app using React and Tailwind CSS with separate user/admin panels, reducing wait times via time-slot booking.
+              • Developed a real-time booking system using calendar and time-picker components with admin-controlled availability.
+              • Improved menu browsing performance by integrating debounced search, API-optimized lazy loading, and paginated rendering.
+              • Streamlined client-server communication via Axios, supporting search, bookings, order updates, and user inquiries through a contact form.
+              </p>
             <div className="technologies">
               <span className="technologyBadge">React.js</span>
               <span className="technologyBadge">Tailwind CSS</span>
@@ -36,15 +39,18 @@ const Experience = () => {
         {/* Experience 2 */}
         <div className="experienceItem">
           <div className="experienceYear">
-            <p className="year">2023-present</p>
+            <p className="year">Apr 2023- jan 2024</p>
           </div>
           <div className="experienceContent">
             <h6 className="experienceRole">
               App Developer - <span className="experienceCompany">INA India Pvt. Ltd</span>
             </h6>
             <p className="experienceDescription">
-              Designed and developed user interfaces for mobile applications using React Native. Collaborated with backend developers to integrate frontend components with Node.js APIs, ensured responsive design, and enhanced frontend performance.
-            </p>
+              • Built a dynamic BMS dashboard in React with Chart.js to visualize real-time cell performance metrics.
+              • Reduced system errors by 40% through robust API integration and input validation layers.
+              • Implemented parameter-based and date-range search with debounced input; optimized data rendering by combining pagination with lazy loading for improved performance and plotted custom datasets using line charts.
+              • Enabled CSV uploads with backend parsing and MySQL storage via custom REST APIs, enhancing data f lexibility.
+              </p>
             <div className="technologies">
               <span className="technologyBadge">React Native</span>
               <span className="technologyBadge">Tailwind CSS</span>
