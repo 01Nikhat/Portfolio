af0a64824d0e8cfc0210681d13234c7e54ededb4
01Nikhat <160391952+01Nikhat@users.noreply.github.com>
2026-05-06 19:53:15 +0530

    Update Intro.jsx

--- src/components/Introduction/Intro.jsx
+++ src/components/Introduction/Intro.jsx
@@ -50,40 +50,43 @@ const Intro = () => {
               initial={{opacity:0,x:-100}}
               transition={{duration:0.5}}
               className="leftSlide">
-            <span className="hello">Hello , I am </span>
+            <span className="hello">Hello, I’m </span>
             </motion.div>
-            <span className="introText">
-              <span className="introName">
-                <Typewriter 
-                  options={{
-                    strings: [
-                      'Nikhat Parvin',
-                      'a Frontend Developer',
-                    ],
-                    autoStart: true,
-                    loop: true,
-                    deleteSpeed: 30,
-                    delay: 75,
-                  }} 
-                />
-              </span>
-            </span>
+          <span className="introText">
+          <span className="introName">Nikhat Parvin</span>
+          <br />
+          <span className="introRole">
+            <Typewriter 
+              options={{
+                strings: [
+                  'Software Developer',
+                  'React Developer',
+                  'Frontend Developer',
+                ],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 30,
+                delay: 75,
+              }} 
+            />
+          </span>
+        </span>
             <motion.div
               whileInView={{opacity:1,x:0}}
               initial={{opacity:0,x:100}}
               transition={{duration:0.5}}
               className="rightSlider">
             <p className="intrDescription">
-              I am a skilled web Developer with Experience in creating visually appealing and user-friendly websites.
+             Building scalable and high-performance web applications using modern technologies.
             </p>
             </motion.div>
             <div className="social">
-              <a href="https://github.com/01Nikhat"><box-icon type='logo' name='github'></box-icon></a>
-              <a href="https://www.linkedin.com/in/nikhat-parvin-8aa676150/"><box-icon type='logo' name='linkedin-square'></box-icon></a>
-              <a href="https://www.facebook.com/nikhat.parvin.12139"><box-icon type='logo' name='facebook-circle'></box-icon></a>
+             <a href="https://github.com/01Nikhat" target="_blank" rel="noreferrer"><box-icon type='logo' name='github'></box-icon></a>
+              <a href="https://www.linkedin.com/in/nikhat-parvin/" target="_blank" rel="noreferrer"><box-icon type='logo' name='linkedin-square'></box-icon></a>
+              
             </div>
             <button className='resumeButton' onClick={handleResumeClick}>
-              <span>RESUME</span> 
+              <span>View Resume</span>
             </button>
             {isMobile && (
               <a href={Resume} className='resumeButton'>
